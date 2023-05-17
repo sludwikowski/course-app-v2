b1f6058b14f09610c08488ab05ff16fc3982c03e
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-05-17 23:36:12 +0200

    build(package): install dependencies

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['cdn.pixabay.com', 'pixabay.com', 'cdn.sanity.io'],
+  },
+}
+
+module.exports = nextConfig
